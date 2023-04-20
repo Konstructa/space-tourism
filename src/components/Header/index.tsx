fe3ea81df12aeb472df8cb02531197864d1e58d5
Konstructa <elenprojects7@gmail.com>
2023-04-20 14:11:40 -0300

    feat: add responsive header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
 import logo from '../../assets/shared/logo.svg'
+import menu from '../../assets/shared/icon-hamburger.svg'
+import close from '../../assets/shared/icon-close.svg'
 import { Link } from 'react-router-dom'
 import './styled.css'
+import { useState } from 'react'
 
 
 const Header = () => {
+
+    const [active, setActive] = useState('menu');
+    const [icon, setIcon] = useState('');
+    
+    const navToggle = () => {
+        active === 'menu' ? setActive('menu menu_active') : setActive('menu')
+
+        icon === '' ? setIcon('active') : setIcon('');
+    }
+
     const nav = [
         {
             number: '00',
@@ -23,16 +36,19 @@ const Header = () => {
     return (
         <div>
             <div className="header">
-                <img src={logo} alt="" className="star" />
-                <div className="content">
-                    <div className="menu">
+                <img src={logo} alt="logo" className="star" />
+                <div className="trace"></div>
+                <div className="content" id={icon}>
+                    <div className={active}>
                         {nav.map((e) => (
                             <div className="nav">
-                                <p style={{ fontWeight: 700 }}>{e.number}</p>
+                                <p className='pNumber'>{e.number}</p>
                                 <p>{e.page}</p>
                             </div>
                         ))}
                     </div>
+                    <img src={menu} onClick={navToggle} alt="menu-hamburger" className='menu-mobile' />
+                    <img src={close} onClick={navToggle} alt="menu-close" className='menu-close' />
                 </div>
             </div>
         </div>
