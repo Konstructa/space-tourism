0848f7ab78b19ea801ebd720df5ac732a5916850
Konstructa <elenprojects7@gmail.com>
2023-06-16 11:40:48 -0300

    Feat: new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import Header from './components/Header'
+import Header from './components/comum/surfaces/Header'
 import Home from './pages/Home'
 import Destination from './pages/Destination';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Container from './components/Container';
-import { useContext } from 'react';
+import Container from './components/comum/container';
 import { BackgroundProvider } from './context/BackgroundContext';
-import BackgroundContext from "./context/BackgroundContext"
+import { Technology } from './pages/Technology';
+import { Crew } from './pages/Crew';
 
 
 
 function App() {
-  
-
   return (
     <BackgroundProvider>
-          <Container >
-            <BrowserRouter>
-              <Header />
-              <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/destination" element={<Destination />} />
-              </Routes>
-            </BrowserRouter>
-        </Container>
+      <Container >
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/destination" element={<Destination />} />
+            <Route path="/crew" element={<Crew/>} />
+            <Route path="/technology" element={<Technology />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </BackgroundProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { GlobalStyles } from './styles/global-style'
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
